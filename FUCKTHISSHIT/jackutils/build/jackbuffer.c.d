d74f1a79e1424ce299ee6b93b0d21e60ac63b8b6
UtoECat <utopia.egor.cat.allandall@gmail.com>
2022-10-22 22:11:52 +0300

    oh you don't see it, ok?

--- FUCKTHISSHIT/jackutils/build/jackbuffer.c.d
+++
@@ -1,7 +0,0 @@
-build/./jackbuffer.c.o: jackbuffer.c \
- /home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackutils.h \
- /home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackbuffer.h \
- /home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackwinutils.h
-/home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackutils.h:
-/home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackbuffer.h:
-/home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackwinutils.h:
--- FUCKTHISSHIT/jackutils/build/jackwin.c.d
+++
@@ -1,7 +0,0 @@
-build/./jackwin.c.o: jackwin.c \
- /home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackutils.h \
- /home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackbuffer.h \
- /home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackwinutils.h
-/home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackutils.h:
-/home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackbuffer.h:
-/home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackwinutils.h:

b1123dd47e02eff14b04be01fcce42ec35d0df94
UtoECat <utopia.egor.cat.allandall@gmail.com>
2022-11-09 07:24:17 +0300

    filter fixes :D

--- jackutils/build/jackutilsport.c.d
+++ jackutils/build/jackutilsport.c.d
@@ -1,15 +1,15 @@
 build/./jackutilsport.c.o: jackutilsport.c \
- /home/utoecat/Projects/jack_utils/include/jackutils.h \
- /home/utoecat/Projects/jack_utils/include/ju_defs.h \
- /home/utoecat/Projects/jack_utils/include/ju_buffer.h \
- /home/utoecat/Projects/jack_utils/include/template/macro.h \
- /home/utoecat/Projects/jack_utils/include/ju_context.h \
- /home/utoecat/Projects/jack_utils/include/ju_port.h \
- /home/utoecat/Projects/jack_utils/include/ju_bits_ctx.h
-/home/utoecat/Projects/jack_utils/include/jackutils.h:
-/home/utoecat/Projects/jack_utils/include/ju_defs.h:
-/home/utoecat/Projects/jack_utils/include/ju_buffer.h:
-/home/utoecat/Projects/jack_utils/include/template/macro.h:
-/home/utoecat/Projects/jack_utils/include/ju_context.h:
-/home/utoecat/Projects/jack_utils/include/ju_port.h:
-/home/utoecat/Projects/jack_utils/include/ju_bits_ctx.h:
+ /home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackutils.h \
+ /home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_defs.h \
+ /home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_buffer.h \
+ /home/utoecat/GitHub/UtoECat/JACKUTILS/include/template/macro.h \
+ /home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_context.h \
+ /home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_port.h \
+ /home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_bits_ctx.h
+/home/utoecat/GitHub/UtoECat/JACKUTILS/include/jackutils.h:
+/home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_defs.h:
+/home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_buffer.h:
+/home/utoecat/GitHub/UtoECat/JACKUTILS/include/template/macro.h:
+/home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_context.h:
+/home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_port.h:
+/home/utoecat/GitHub/UtoECat/JACKUTILS/include/ju_bits_ctx.h:
